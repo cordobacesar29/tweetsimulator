eea76d99dc31164d2ae6aed144a476c1f84ba336
cesarcordoba <cordobacesar29@gmail.com>
2025-05-30 23:44:09 -0300

    TweetList template

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <Menu :openCloseForm="openCloseForm" :showForm="showForm" />
   <TweetForm :showForm="showForm"/>
+  <TweetList />
 </template>
 
 <script>
 import Menu from "./components/Menu.vue";
 import TweetForm from "./components/TweetForm.vue";
+import TweetList from "./components/TweetList.vue";
 import useFormTweet from "./hooks/useFormTweet"
 
 export default {
@@ -13,6 +15,7 @@ export default {
   components: {
     Menu,
     TweetForm,
+    TweetList
   },
   setup() {
     return {
