25c0c016de12780a9fae80b38b858d028492721c
cesarcordoba <cordobacesar29@gmail.com>
2025-05-30 23:31:48 -0300

    lodash

--- src/api/tweet.js
+++ src/api/tweet.js
@@ -1,16 +1,28 @@
 import { v4 as uuidV4 } from "uuid";
 import { TWEETS } from "../utils/constants";
+import {size} from "lodash"
 
 export function saveTweetApi(tweet, userName) {
-    const tweets = getTweetsApi();
+  const tweets = getTweetsApi();
 
-  const tweetTemp = {
-    id: uuidV4(),
-    tweet,
-    userName,
-    createAt: new Date(),
-  };
-  localStorage.setItem(TWEETS, JSON.stringify(tweetTemp));
+  if (size(tweets) === 0) {
+    const tweetTemp = [
+      {
+        id: uuidV4(),
+        tweet,
+        userName,
+        createdAt: new Date(),
+      },
+    ];
+    localStorage.setItem(TWEETS, JSON.stringify(tweetTemp));
+  } else {
+    tweets.push({
+      id: uuidV4(),
+      tweet,
+      userName,
+      createdAt: new Date(),
+    });
+  }
 }
 
 export function getTweetsApi() {
